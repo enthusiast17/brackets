4a36e3923969cf889ef409137974303e8371bf6a
enthusiast17 <ulan17nurym@gmail.com>
2020-10-06 19:51:29 +0500

    feat: add brackets solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    const {def, cfg} = bracketsConfig.reduce((res, [a, b]) => {
+        const def = {}, cfg = {}
+        if (a === b) def[a] = 'the same'
+        else def[a] = 'open', def[b] = 'close'
+        cfg[a] = b, cfg[b] = a
+        Object.assign(res.def, def), Object.assign(res.cfg, cfg)
+        return res
+    }, {def: {}, cfg: {}})
+    const arr = [...str]
+    const iter = (arr, acc) => {
+        if (acc >= arr.length || arr.length == 0) return arr
+        if (def[arr[acc]] !== 'close' && arr.length > acc + 1 && cfg[arr[acc]] === arr[acc + 1]) {
+            arr.splice(acc, 2)
+            acc = -1
+        }
+        return iter(arr, acc + 1)
+    }
+    return iter(arr, 0).length === 0
 }
